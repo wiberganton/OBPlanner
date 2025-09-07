5f3101fde0b19b1b657751fd641deb5e7eb6b93d
Kallefinn <tinchuk@gmx.de>
2025-09-07 16:33:43 +0200

    testing stuff in example.py

--- examples/examples.py
+++ examples/examples.py
@@ -5,13 +5,12 @@ from obplanner.model.layer_default import LayerDefault, LayerStrategies
 from obplanner.model.build import Build
 from os.path import normpath as normalized_path
 
-components = [0, 1, 2]
 
 my_pattern_settings = pattern.PatternSettings(
     point_distance = 1,
     type = "square",
     offset = 0.0, # Offset distance compared against 3mf file contour
-    start_rotation = 0.0, # Rotation angle in degrees for first layer
+    start_rotation = 90.0, # Rotation angle in degrees for first layer
     layer_rotation = 0.0)  # Rotation angle in degrees between layers)
     
 
@@ -19,9 +18,13 @@ my_pattern_settings = pattern.PatternSettings(
 geometry1 = normalized_path("tests/geometries/test_geometry1.stl")
 geometry2 = normalized_path("tests/geometries/test_geometry2.stl")
 geometry3 = normalized_path("tests/geometries/test_geometry3.stl")
+
+geometry_list = [geometry1, geometry2, geometry3]
+components = list(range(len(geometry_list))) #passes in a list in the form [0,1,2,3...n]
+
 import py3mf_slicer.load
 import py3mf_slicer.slice
-model = py3mf_slicer.load.load_files([geometry1, geometry2, geometry3])
+model = py3mf_slicer.load.load_files(geometry_list)
 sliced_model = py3mf_slicer.slice.slice_model(model, 1)
 
 pattern = generator.generate_pattern(sliced_model, 0, components, my_pattern_settings)
@@ -31,19 +34,30 @@ strategy = Strategy(
     pattern = my_pattern_settings, # Which pattern that should be used
     strategy = "LineSort", # defining which strategy that is used (the different strategies are defined in docs/scanning_strategies.md)
     power = 660, # Watt
-    spot_size = 150, # in um 
+    spot_size = 50, # in um 
     speed = 100000, # in um/s
     dwell_time = 10000 # in ns
 )
 
+strategy2 = Strategy(
+    geometry = components,
+    pattern = my_pattern_settings, 
+    strategy = "SpotRandom", 
+    power = 660,
+    spot_size = 50, 
+    speed = 100000,
+    dwell_time = 10000 
+)
 layer_strat = LayerStrategies(
-    melt = [strategy, strategy]
+    melt = [strategy, strategy2] 
 )
 
 from obplanner.model.single_file import SingleShape
 
 shape = SingleShape(
-    [strategy]
+    strategies = [strategy,strategy2],
+    shape = my_pattern_settings.type,
+    size = 500
 )
 
 from obplanner.model.layer_default import LayerDefault, StartHeat
